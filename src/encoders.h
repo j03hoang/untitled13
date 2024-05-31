0eac66bdb0c6e60dcf6118b9420e3e09a609d00e
josep <joseph.hoang53@gmail.com>
2024-05-31 11:59:53 -0700

    sensor

--- src/encoders.h
+++ src/encoders.h
@@ -1,9 +1,22 @@
 #ifndef UNTITLED13_ENCODERS_H
 #define UNTITLED13_ENCODERS_H
 
+#include <RotaryEncoder.h>
+#include "config.h"
 
-class encoders {
+RotaryEncoder encoder(ENCODER_PIN_1, ENCODER_PIN_2, RotaryEncoder::LatchMode::TWO03);
 
+class Encoders {
+    public:
+     void init() {
+
+     }
+
+    private:
+     float m_robot_distance;
+     float m_robot_angle;
+     float m_fwd_change;
+     float m_rot_change;
 };
 
 
--- src/sensors.h
+++ src/sensors.h
@@ -1,8 +1,40 @@
 #ifndef UNTITLED13_SENSORS_H
 #define UNTITLED13_SENSORS_H
 
+#include "config.h"
 
-class sensors {
+// this class should give steering adjustment
+// should tell if there's a wall
+
+class Sensors {
+    public:
+
+     bool see_front_wall;
+     bool see_left_wall;
+     bool see_right_wall;
+
+     void init() {
+         pinMode(ULTRASONIC_TRIG_1, OUTPUT);
+         pinMode(ULTRASONIC_ECHO_1, INPUT);
+     }
+
+     int getDistance() {
+         long duration, distance;
+         digitalWrite(ULTRASONIC_TRIG_1, LOW);
+         delayMicroseconds(2);
+         digitalWrite(ULTRASONIC_TRIG_1, HIGH);
+         delayMicroseconds(10);
+         digitalWrite(ULTRASONIC_ECHO_1, LOW);
+         duration = pulseIn(ULTRASONIC_ECHO_1, HIGH);
+         distance = (duration/2) / 29.1;
+
+         delay(100);
+
+         return distance;
+    }
+
+
+    private:
 
 };
 
